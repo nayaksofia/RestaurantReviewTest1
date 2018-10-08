02993c2f371b1aa62311d6b6e304f727ca706446
nayaksofia <nayaksofia1989@gmail.com>
2018-10-08 18:52:20 +0530

    Install service worker successfully

--- sw.js
+++ sw.js
@@ -1,34 +1,34 @@
 //Install a service worker
 
-// var CACHE_NAME = 'my-site-cache-v1';
-// var urlsToCache = [
-//     '/',
-//     '/index.html',
-//     '/restaurant.html',
-//     '/css/styles.css',
-//     '/js/dbhelper.js',
-//     '/js/main.js',
-//     '/js/restaurant_info.js',
-//     '/data/restaurants.json',
-//     '/img/1.jpg',
-//     '/img/2.jpg',
-//     '/img/3.jpg',
-//     '/img/4.jpg',
-//     '/img/5.jpg',
-//     '/img/6.jpg',
-//     '/img/7.jpg',
-//     '/img/8.jpg',
-//     '/img/9.jpg',
-//     '/img/10.jpg'
-// ];
+var CACHE_VERSION = 'my-restuarant-cache-v1';
+var urlsToCache = [
+    '/',
+    '/index.html',
+    '/restaurant.html',
+    '/css/styles.css',
+    '/js/dbhelper.js',
+    '/js/main.js',
+    '/js/restaurant_info.js',
+    '/data/restaurants.json',
+    '/img/1.jpg',
+    '/img/2.jpg',
+    '/img/3.jpg',
+    '/img/4.jpg',
+    '/img/5.jpg',
+    '/img/6.jpg',
+    '/img/7.jpg',
+    '/img/8.jpg',
+    '/img/9.jpg',
+    '/img/10.jpg'
+];
 
-// self.addEventListener('install', function(event) {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_VERSION)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
