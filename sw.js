1a79db340f6a57dc69b4fea48afad9561ce246fa
nayaksofia <nayaksofia1989@gmail.com>
2018-10-08 23:13:50 +0530

    Activate service worker

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 //Install a service worker
 
-var CACHE_VERSION = 'my-restuarant-cache-v1';
+var CACHE_NAME = 'my-restuarant-cache-v1';
 var urlsToCache = [
     '/',
     '/index.html',
@@ -25,10 +25,62 @@ var urlsToCache = [
 self.addEventListener('install', function(event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_VERSION)
+    caches.open(CACHE_NAME)
       .then(function(cache) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
 });
+
+//Activate 
+self.addEventListener('activate', function(event) {
+
+  // var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+  console.log("Service worker activated");
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+//Cache and Return Request 
+
+// self.addEventListener('fetch', function(event) {
+//     event.respondWith(
+//       caches.match(event.request)
+//         .then(function(response) {
+//           // Cache hit - return response
+//           if (response) {
+//             return response;
+//           }
+//           var fetchRequest = event.request.clone();
+  
+//           return fetch(fetchRequest).then(
+//             function(response) {
+//               // Check if we received a valid response
+//               if(!response || response.status !== 200 || response.type !== 'basic') {
+//                 return response;
+//               }
+
+//               var responseToCache = response.clone();
+  
+//               caches.open(CACHE_NAME)
+//                 .then(function(cache) {
+//                   cache.put(event.request, responseToCache);
+//                 });
+  
+//               return response;
+//             }
+//           );
+//         })
+//       );
+//   });
