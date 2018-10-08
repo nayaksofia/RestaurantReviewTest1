24ed898bc306fcda51786de3861689c3e5b67496
nayaksofia <nayaksofia1989@gmail.com>
2018-10-08 18:45:25 +0530

    Service worker registration successful

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 // Register the service worker 
+
 // if ('serviceWorker' in navigator) {
 //   navigator.serviceWorker
 //   .register('/sw.js')
@@ -6,6 +7,18 @@
 //     console.error(err);
 //   });
 // }
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+} 
 
 let restaurants,
   neighborhoods,
