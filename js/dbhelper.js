a3ab6f5cda0bb4e1fe99811eabce80633e9a2b76
nayaksofia <nayaksofia1989@gmail.com>
2018-10-09 10:22:00 +0530

    Change the path of image sources

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -152,7 +152,8 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    //return (`/img/${restaurant.photograph}`);
+    return (`https://nayaksofia.github.io/RestaurantReviewTest1/img/${restaurant.photograph}`);
   }
 
   /**
